2462b8ff3419b9685313d05a6e397d062b30a847
Raj <raj.rajeev2244@gmail.com>
2021-01-08 22:43:16 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/DemoRestController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/hello")
+public class DemoRestController {
+
+	@GetMapping(value = "/welcome")
+	public String welcomePage()
+	{
+		String msg="<h1>Welcome To Spring Boot App</h1>";
+		return msg;
+	}
+	
+}
